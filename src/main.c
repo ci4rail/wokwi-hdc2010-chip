66261ca8c5eb4beed7aae3a6e1dc5338381588ea
popples <klauspopp@gmx.de>
2025-01-25 19:03:29 +0000

    WIP

--- src/main.c
+++ src/main.c
@@ -1,30 +1,117 @@
-// For information and examples see:
+// Wokwi Custom Chip - For information and examples see:
 // https://link.wokwi.com/custom-chips-alpha
+//
+// SPDX-License-Identifier: MIT
+// Copyright (C) 2022 Uri Shaked / wokwi.com
 
 #include "wokwi-api.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define REG_SIZE 0x100
+
+const int BASE_ADDRESS = 0x40;
+
+typedef enum {
+  IDLE,
+  ADDRESS_1,
+  DATA
+} chip_i2c_state_t;
 
 typedef struct {
-  pin_t pin_in;
-  pin_t pin_out;
+  chip_i2c_state_t state;
+  pin_t pin_addr;
+  pin_t pin_drdy;
+  uint8_t address_register;
+  uint8_t regs[REG_SIZE];
 } chip_state_t;
 
-static void chip_pin_change(void *user_data, pin_t pin, uint32_t value) {
-  chip_state_t *chip = (chip_state_t*)user_data;
-  printf("Pin change: %d %d\n", pin, value);
-  pin_write(chip->pin_out, !value);
+
+static bool on_i2c_connect(void *user_data, uint32_t address, bool connect);
+static uint8_t on_i2c_read(void *user_data);
+static bool on_i2c_write(void *user_data, uint8_t data);
+static void on_i2c_disconnect(void *user_data);
+
+
+static uint8_t get_addr(chip_state_t *chip) {
+  uint8_t address = BASE_ADDRESS;
+  address |= pin_read(chip->pin_addr);
+  return address;
+}
+
+static bool is_readonly(uint8_t address) {
+  if (address >= 0x4 && address <= 0xf) {
+    return false;
+  }
+  return true;
 }
 
 void chip_init(void) {
   chip_state_t *chip = malloc(sizeof(chip_state_t));
-  chip->pin_in = pin_init("IN", INPUT);
-  chip->pin_out = pin_init("OUT", OUTPUT);
+  chip->pin_addr = pin_init("ADDR", INPUT);
+  chip->pin_drdy = pin_init("DRDY", OUTPUT);
+  uint8_t address = get_addr(chip);
 
-  const pin_watch_config_t config = {
-    .edge = BOTH,
-    .pin_change = chip_pin_change,
+  chip->address_register = 0;
+  memset(chip->regs, 0, REG_SIZE);
+  chip->regs[0xfc] = 0x49;
+  chip->regs[0xfd] = 0x54;
+  chip->regs[0xfe] = 0xd0;
+  chip->regs[0xff] = 0x07;
+
+  chip->state = IDLE;
+  const i2c_config_t i2c_config = {
     .user_data = chip,
+    .address = address,
+    .scl = pin_init("SCL", INPUT),
+    .sda = pin_init("SDA", INPUT),
+    .connect = on_i2c_connect,
+    .read = on_i2c_read,
+    .write = on_i2c_write,
+    .disconnect = on_i2c_disconnect, // Optional
   };
-  pin_watch(chip->pin_in, &config);
+  i2c_init(&i2c_config);
+
+  // The following message will appear in the browser's DevTools console:
+  printf("Hello from HDC2010 at address %x \n", address);
+}
+
+bool on_i2c_connect(void *user_data, uint32_t address, bool connect) {
+  return true; /* Ack */
+}
+
+uint8_t on_i2c_read(void *user_data) {
+  chip_state_t *chip = user_data;
+  uint8_t data = chip->regs[chip->address_register];
+  chip->address_register = (chip->address_register + 1) & 0xff;
+  return data;
+}
+
+bool on_i2c_write(void *user_data, uint8_t data) {
+  chip_state_t *chip = user_data;
+  switch(chip->state)
+  {
+    case IDLE:
+      chip->address_register = data;
+      chip->state = DATA;
+    break;
+    case DATA:
+      if (is_readonly(chip->address_register)) {
+        return false; // Nack
+      }
+      printf("Writing to register %x: %x\n", chip->address_register, data);
+      chip->regs[chip->address_register] = data;
+      chip->address_register = (chip->address_register + 1) & 0xff;
+    break;
+    default:
+      printf("error");
+    break;
+  }
+    return true; // Ack
+}
+
+void on_i2c_disconnect(void *user_data) {
+  chip_state_t *chip = user_data;
+  chip->state = IDLE;
 }
